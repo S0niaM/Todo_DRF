3647d510f80fd7a9b13ea58acc7676ac8c46d285
S0niaM <145560333+S0niaM@users.noreply.github.com>
2024-07-08 15:49:23 +0530

    changed the structure
    changed the structure

---
+++ todo-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import TaskList from './components/TaskList';
+import './fonts.css';
+
+import './index.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
